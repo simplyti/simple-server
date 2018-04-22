00fbd55fe8ad6506af54ab720fd371298d136da5
Pablo Táboas <pablo.taboas@bbva.com>
2018-04-23 00:18:31 +0200

    Remove unready jet auth module No deploy acceptance module

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@
 openssl aes-256-cbc -K $encrypted_1ed45ccca01f_key -iv $encrypted_1ed45ccca01f_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
-mvn --settings=./.travis/settings.xml compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
+mvn --settings=./.travis/settings.xml -pl !acceptance compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
