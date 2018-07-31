6d0b5c67067e6eb9e6a08e1a84ca47759a5a7800
Pablo Táboas <pablo.taboas@bbva.com>
2018-08-01 00:17:08 +0200

    refact: server sent event message coding

---
+++ core/src/main/java/com/simplyti/service/sse/ServerEvent.java
@@ -0,0 +1,16 @@
+package com.simplyti.service.sse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent=true)
+@AllArgsConstructor
+public class ServerEvent {
+
+	private final String event;
+	private final String id;
+	private final String data;
+	
+}
