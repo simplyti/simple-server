c00cdcf17ab9ccfe9faa710b3d086c05bf54e542
Pablo Táboas <pablo.taboas@bbva.com>
2018-12-03 18:18:44 +0100

    http client: do not encode query param name

--- clients-http/src/main/java/com/simplyti/service/clients/http/request/AbstractFinishableHttpRequest.java
+++ clients-http/src/main/java/com/simplyti/service/clients/http/request/AbstractFinishableHttpRequest.java
@@ -1,7 +1,11 @@
 package com.simplyti.service.clients.http.request;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -17,8 +21,8 @@ import com.simplyti.service.clients.http.sse.ServerEvent;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpConstants;
 import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.QueryStringEncoder;
 import io.netty.util.concurrent.Future;
 
 public abstract class AbstractFinishableHttpRequest implements FinishableHttpRequest {
@@ -26,6 +30,7 @@ public abstract class AbstractFinishableHttpRequest implements FinishableHttpReq
 	protected final InternalClient client;
 	private final boolean checkStatusCode;
 	private final ClientConfig config;
+	private final String charsetName;
 	
 	private final Map<String,Object> params;
 	
@@ -34,6 +39,7 @@ public abstract class AbstractFinishableHttpRequest implements FinishableHttpReq
 		this.config=config;
 		this.checkStatusCode=checkStatusCode;
 		this.params=new HashMap<>();
+		this.charsetName=HttpConstants.DEFAULT_CHARSET.name();
 	}
 	
 	@Override
@@ -95,10 +101,51 @@ public abstract class AbstractFinishableHttpRequest implements FinishableHttpReq
 		}
 		
 		FullHttpRequest request = request0();
-		QueryStringEncoder encoder = new QueryStringEncoder(request.uri());
-		params.forEach((name,value)->encoder.addParam(name, value!=null?value.toString():null));
-		return request.setUri(encoder.toString());
+		final StringBuilder uriBuilder = new StringBuilder(request.uri());
+		boolean hasParams = false;
+		for(Entry<String,Object> entry:params.entrySet()) {
+			if (hasParams) {
+	            uriBuilder.append('&');
+	        } else {
+	            uriBuilder.append('?');
+	            hasParams = true;
+	        }
+			appendComponent(entry.getKey(), charsetName, uriBuilder,false);
+			if (entry.getValue() != null) {
+	            uriBuilder.append('=');
+	            appendComponent(entry.getValue().toString(), charsetName, uriBuilder,true);
+	        }
+		}
+		
+		return request.setUri(uriBuilder.toString());
 	}
+	
+	private static void appendComponent(String s, String charset, StringBuilder sb, boolean urlEncode) {
+		if(urlEncode) {
+			try {
+	            s = URLEncoder.encode(s, charset);
+	        } catch (UnsupportedEncodingException ignored) {
+	            throw new UnsupportedCharsetException(charset);
+	        }
+		}
+        // replace all '+' with "%20"
+        int idx = s.indexOf('+');
+        if (idx == -1) {
+            sb.append(s);
+            return;
+        }
+        sb.append(s, 0, idx).append("%20");
+        int size = s.length();
+        idx++;
+        for (; idx < size; idx++) {
+            char c = s.charAt(idx);
+            if (c != '+') {
+                sb.append(c);
+            } else {
+                sb.append("%20");
+            }
+        }
+    }
 	
 	protected abstract FullHttpRequest request0();
 
