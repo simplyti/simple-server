66b4fac79775647a56a1c40a3a2102306f3d5d92
Pablo Taboas <pablo.taboas@bbva.com>
2019-06-23 11:37:44 +0200

    test: add missing resource

---
+++ acceptance/src/test/java/com/simplyti/service/StreamAPITest.java
@@ -0,0 +1,35 @@
+package com.simplyti.service;
+
+import com.simplyti.service.api.builder.ApiBuilder;
+import com.simplyti.service.api.builder.ApiProvider;
+
+import io.netty.handler.codec.http.DefaultHttpContent;
+import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
+
+public class StreamAPITest implements ApiProvider{
+	
+
+	@Override
+	public void build(ApiBuilder builder) {
+		builder.when().post("/streamed")
+			.streamedInput()
+			.then(ctx->{
+				HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK);
+				response.headers().set(HttpHeaderNames.CONTENT_LENGTH,ctx.request().headers().get(HttpHeaderNames.CONTENT_LENGTH));
+				ctx.send(response);
+				ctx.stream(content->{
+					ctx.send(new DefaultHttpContent(content).retain());
+				})
+					.addListener(f->{
+						ctx.send(LastHttpContent.EMPTY_LAST_CONTENT);
+					});
+			});
+		
+	}
+
+}
