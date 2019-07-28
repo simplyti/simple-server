cfae9025c3cdf8bdd4e557602976bf6a4ace0c07
Pablo Taboas <pablo.taboas@bbva.com>
2019-07-28 15:28:12 +0200

    add dsljson annotation

--- auth-oidc/src/main/java/com/simplyti/service/security/oidc/callback/State.java
+++ auth-oidc/src/main/java/com/simplyti/service/security/oidc/callback/State.java
@@ -2,13 +2,11 @@ package com.simplyti.service.security.oidc.callback;
 
 import com.dslplatform.json.CompiledJson;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Accessors(fluent=true)
 @Getter
-@AllArgsConstructor(onConstructor=@__(@CompiledJson))
 public class State {
 	
 	private final String clientId;
@@ -16,4 +14,11 @@ public class State {
 	private final String redirectUri;
 	private final String tokenEndpoint;
 
+	@CompiledJson
+	public State(String clientId,String clientSecret,String redirectUri,String tokenEndpoint) {
+		this.clientId=clientId;
+		this.clientSecret=clientSecret;
+		this.redirectUri=redirectUri;
+		this.tokenEndpoint=tokenEndpoint;
+	}
 }
