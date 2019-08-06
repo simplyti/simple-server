7b4cc9823aa5b404def545eddec3db3df68ce747
Pablo Taboas <pablo.taboas@bbva.com>
2019-08-06 15:45:04 +0200

    chore: update gpg keys

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_1ed45ccca01f_key -iv $encrypted_1ed45ccca01f_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_12c8071d2874_key -iv $encrypted_12c8071d2874_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
 mvn --settings=./.travis/settings.xml -pl !acceptance compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
