3a7dd0a3dcc02331baa11615862504b5b7d2ceb1
Guillermo Priego Blanco <guillermo.priego.next@bbva.com>
2019-09-20 17:42:43 +0200

    get running from container state

---
+++ clients-k8s/src/main/java/com/simplyti/service/clients/k8s/pods/domain/ContainerStateRunning.java
@@ -0,0 +1,11 @@
+package com.simplyti.service.clients.k8s.pods.domain;
+
+import com.dslplatform.json.CompiledJson;
+
+public class ContainerStateRunning {
+
+    @CompiledJson
+    public ContainerStateRunning() {
+
+    }
+}
