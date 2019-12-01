d116b03c7aa4443dd3e0f766196aeb91b3c1264c
Pablo Taboas <pablo.taboas@bbva.com>
2019-12-01 01:43:25 +0100

    feat: vitaminized future

---
+++ commons-netty/src/main/java/com/simplyti/util/concurrent/BiCombinedFuture.java
@@ -0,0 +1,20 @@
+package com.simplyti.util.concurrent;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public interface BiCombinedFuture<T, U> {
+	
+	Future<Void> thenAccept(BiConsumer<? super T,? super U> action);
+	
+	<O> Future<O> thenApply(BiFunction<? super T,? super U, ? extends O> action);
+	
+	<O> Future<O> thenCombine(BiFunction<? super T,? super U, io.netty.util.concurrent.Future<O>> fn);
+
+	<O> Future<O> exceptionallyApply(Function<Throwable, ? extends O> fn);
+	
+	Future<Void> onError(Consumer<Throwable> action);
+
+}
---
+++ commons-netty/src/main/java/com/simplyti/util/concurrent/DefaultBiCombinedFuture.java
@@ -0,0 +1,42 @@
+package com.simplyti.util.concurrent;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import io.netty.util.concurrent.EventExecutor;
+
+public class DefaultBiCombinedFuture<T,U> implements BiCombinedFuture<T,U>{
+	
+	private final Future<Object[]> target;
+
+	public DefaultBiCombinedFuture(io.netty.util.concurrent.Future<Object[]> target, EventExecutor loop) {
+		this.target=new DefaultFuture<>(target, loop);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Future<Void> thenAccept(BiConsumer<? super T,? super U> action){
+		return target.thenAccept(arr->action.accept((T)arr[0], (U)arr[1]));
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <O> Future<O> thenApply(BiFunction<? super T,? super U, ? extends O> action) {
+		return target.thenApply(arr->action.apply((T)arr[0], (U)arr[1]));
+	}
+
+	public <O> Future<O> exceptionallyApply(Function<Throwable, ? extends O> fn) {
+		return target.exceptionallyApply(fn);
+	}
+	
+	public Future<Void> onError(Consumer<Throwable> action){
+		return target.onError(action);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <O> Future<O> thenCombine(BiFunction<? super T, ? super U, io.netty.util.concurrent.Future<O>> fn) {
+		return target.thenCombine(arr->fn.apply((T)arr[0], (U)arr[1]));
+	}
+
+}
