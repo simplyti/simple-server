97f14dd7d3e51e69b69ca78ae03f31c3c0a0988e
Pablo Taboas <pablo.taboas@bbva.com>
2020-05-12 00:45:36 +0200

    feat: client: http: support http compressed responses

--- clients-http/src/main/java/com/simplyti/service/clients/http/channel/HttpChannelInitializer.java
+++ clients-http/src/main/java/com/simplyti/service/clients/http/channel/HttpChannelInitializer.java
@@ -3,11 +3,13 @@ package com.simplyti.service.clients.http.channel;
 import io.netty.channel.Channel;
 import io.netty.channel.pool.AbstractChannelPoolHandler;
 import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpContentDecompressor;
 
 public class HttpChannelInitializer extends AbstractChannelPoolHandler{
 	
 	@Override
 	public void channelCreated(Channel ch) throws Exception {
 		ch.pipeline().addLast(new HttpClientCodec());
+		ch.pipeline().addLast(new HttpContentDecompressor());
 	}
 }
