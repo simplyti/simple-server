791aa536a343fd78c4234c55663f0da42f873989
Pablo Taboas <pablo.taboas@bbva.com>
2021-07-25 11:38:35 +0200

    feat(multipart api): update itests

--- acceptance/src/test/java/com/simplyti/service/examples/api/APIUploadTest.java
+++ acceptance/src/test/java/com/simplyti/service/examples/api/APIUploadTest.java
@@ -1,6 +1,5 @@
 package com.simplyti.service.examples.api;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,7 +7,6 @@ import com.simplyti.service.api.builder.ApiBuilder;
 import com.simplyti.service.api.builder.ApiProvider;
 
 import io.netty.handler.codec.http.multipart.FileUpload;
-import io.netty.handler.codec.http.multipart.InterfaceHttpData;
 
 public class APIUploadTest implements ApiProvider{
 
@@ -17,15 +15,12 @@ public class APIUploadTest implements ApiProvider{
 		builder.when().post("/upload")
 			.asFileUpload()
 			.then(ctx->{
-				List<InterfaceHttpData> parts = new ArrayList<>();
-				ctx.eachPart(parts::add)
-					.thenApply(v->parts.stream()
-							.filter(p->p instanceof FileUpload)
-							.map(FileUpload.class::cast)
-							.map(file->file.getName())
-							.collect(Collectors.toList()))
-					.thenAccept(l->ctx.send("Got "+l))
-					.exceptionally(ctx::failure);
+				List<String> list = ctx.files().stream()
+					.filter(p->p instanceof FileUpload)
+					.map(FileUpload.class::cast)
+					.map(file->file.getName())
+					.collect(Collectors.toList());
+				ctx.send("Got "+list);
 			});
 	}
 
