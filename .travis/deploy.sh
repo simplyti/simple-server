fc24ec530eb93931c3771c18265cb9c5918a262a
Pablo Taboas <pablo.taboas@bbva.com>
2021-08-10 16:57:45 +0200

    chore: quiet maven deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@
 openssl aes-256-cbc -K $encrypted_25a884814f46_key -iv $encrypted_25a884814f46_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
-mvn --settings=./.travis/settings.xml -pl !acceptance compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
+mvn --quiet --settings=./.travis/settings.xml -pl !acceptance compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
