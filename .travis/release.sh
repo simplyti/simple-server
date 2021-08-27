b62e9191441cd885a75a3e813fec8bc12621f678
Pablo Taboas <pablo.taboas@bbva.com>
2021-08-27 16:30:25 +0200

    chore: quiet release execution

--- .travis/release.sh
+++ .travis/release.sh
@@ -7,4 +7,4 @@ mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
 openssl aes-256-cbc -K $encrypted_25a884814f46_key -iv $encrypted_25a884814f46_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
-mvn --settings=./.travis/settings.xml -pl !acceptance compile jar:jar source:jar-no-fork javadoc:jar gpg:sign nexus-staging:deploy
+mvn --quiet --settings=./.travis/settings.xml -pl !acceptance compile jar:jar source:jar-no-fork javadoc:jar gpg:sign nexus-staging:deploy
